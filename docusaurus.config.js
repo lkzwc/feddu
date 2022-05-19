3531869319929254d5a735afa82164738247ba3c
lkzwc <lkzwc@outlook.com>
2022-05-19 23:34:54 +0800

    [update default theme]

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,6 +54,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: "张文超-个人博客",
         logo: {

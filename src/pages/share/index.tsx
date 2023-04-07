a67987f6af9a19f9489ad56082fc69b4ed31117d
流客 <lkzwc@outlook.com>
2023-04-07 14:16:52 +0800

    2

--- src/pages/share/index.tsx
+++ src/pages/share/index.tsx
@@ -9,14 +9,17 @@ function Classify(props) {
         {
           name: "cdn",
           url: "https://cdnjs.com/libraries/font-awesome",
-        },
-        {
-          name: "cdn",
-          url: "https://cdnjs.com/libraries/font-awesome",
+          desc:'简单的图标库，通过引入CDN样式以及使用简单的<i>即可实现'
         },
         {
           name:'gif'
+          desc:'GIF 图片搜索'
           url: 'https://gifdb.com/'
+        }
+        {
+          name:'gif',
+          desc: "提供CDN链接，速度快",
+          url: 'https://dribbble.com/'
         }
       ],
     },

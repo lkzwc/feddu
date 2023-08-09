551bc89595f5e424efa1e82a097ad64c644f3977
流客 <lkzwc@outlook.com>
2023-08-09 17:35:44 +0800

    update

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -2,24 +2,45 @@
 // import BlogPostItem from "@theme-original/BlogPostItem";
 import React from 'react';
 import clsx from 'clsx';
-import { useBlogPost} from '@docusaurus/theme-common/internal';
 import BlogPostItemContainer from '@theme/BlogPostItem/Container';
 import BlogPostItemHeader from '@theme/BlogPostItem/Header';
 import BlogPostItemContent from '@theme/BlogPostItem/Content';
 import BlogPostItemFooter from '@theme/BlogPostItem/Footer';
 import type {Props} from '@theme/BlogPostItem';
 
-// apply a bottom margin in list view
-function useContainerClassName() {
-  const {isBlogPostPage} = useBlogPost();
-  return !isBlogPostPage ? 'margin-bottom--xl' : undefined;
-}
-
-export default function BlogPostItem({
-  children,
-  className,
-}: Props): JSX.Element {
+export default function BlogPostItem(props) {
   const containerClassName = useContainerClassName();
+  const {
+    children,
+    frontMatter,
+    metadata,
+    truncated,
+    isBlogPostPage = false,
+    views,
+  } = props;
+
+  const { date, permalink, tags, readingTime } = metadata;
+
+  // activityId, oid 为 B 站评论相关
+  const {
+    slug: postId,
+    author,
+    title,
+    image,
+    activityId,
+    oid,
+    bvid,
+  } = frontMatter;
+
+  const authorURL = frontMatter.author_url || frontMatter.authorURL;
+  const authorTitle = frontMatter.author_title || frontMatter.authorTitle;
+  const authorImageURL =
+    frontMatter.author_image_url || frontMatter.authorImageURL;
+  const imageUrl = useBaseUrl(image, { absolute: true });
+
+  // 是否为黑暗主题：
+  const theme = useContext(ThemeContext);
+  const { isDarkTheme } = theme;
   return (
     <BlogPostItemContainer className={clsx('margin-bottom--xl', className)}>
       <script src="https://giscus.app/client.js"

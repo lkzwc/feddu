746501573d6f8f895e7b1b17a28b719c352e03fa
lkzwc <lkzwc@outlook.com>
2023-08-09 08:41:38 +0000

    gitcus

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -2,7 +2,7 @@
 // import BlogPostItem from "@theme-original/BlogPostItem";
 import React from 'react';
 import clsx from 'clsx';
-// import {useBlogPost} from '@docusaurus/theme-common/internal';
+import { useBlogPost} from '@docusaurus/theme-common/internal';
 import BlogPostItemContainer from '@theme/BlogPostItem/Container';
 import BlogPostItemHeader from '@theme/BlogPostItem/Header';
 import BlogPostItemContent from '@theme/BlogPostItem/Content';
@@ -11,8 +11,8 @@ import type {Props} from '@theme/BlogPostItem';
 
 // apply a bottom margin in list view
 function useContainerClassName() {
-  // const {isBlogPostPage} = useBlogPost();
-  // return !isBlogPostPage ? 'margin-bottom--xl' : undefined;
+  const {isBlogPostPage} = useBlogPost();
+  return !isBlogPostPage ? 'margin-bottom--xl' : undefined;
 }
 
 export default function BlogPostItem({
@@ -22,6 +22,22 @@ export default function BlogPostItem({
   const containerClassName = useContainerClassName();
   return (
     <BlogPostItemContainer className={clsx('margin-bottom--xl', className)}>
+      <script src="https://giscus.app/client.js"
+        data-repo="lkzwc/fe-ddu"
+        data-repo-id="R_kgDOHUkdvQ"
+        data-category="BLOG"
+        data-category-id="DIC_kwDOHUkdvc4CYeix"
+        data-mapping="title"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossOrigin="anonymous"
+        async>
+      </script>
       <BlogPostItemHeader />
       <BlogPostItemContent>{children}</BlogPostItemContent>
       <BlogPostItemFooter />
